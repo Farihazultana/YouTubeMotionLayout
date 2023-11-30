829ed8a16609945289ea74274c0cedd0d2fb8220
farihazultana <farihasultana237@gmail.com>
2023-11-30 15:12:04 +0600

    End point

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.motionlayout"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.motionlayout"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
