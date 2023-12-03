84be922e525c7895c21328d44b82e8afa2eee94d
farihazultana <farihasultana237@gmail.com>
2023-12-03 11:20:10 +0600

    down icon added & fragment transition added

--- app/src/main/java/com/example/motionlayout/MainActivity.kt
+++ app/src/main/java/com/example/motionlayout/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.motionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    //binding.cvMiniPlayer.visibility = View.VISIBLE
+                    replaceFragment(HomeFragment())
+                }
+                R.id.audio -> {
+                    //binding.cvMiniPlayer.visibility = View.VISIBLE
+                    replaceFragment(AudioFragment())
+                }
+                R.id.video -> {
+                    //binding.cvMiniPlayer.visibility = View.VISIBLE
+                    replaceFragment(VideoFragment())
+                }
+                R.id.news -> {
+                    //binding.cvMiniPlayer.visibility = View.VISIBLE
+                    replaceFragment(NewsFragment())
+                }
+                R.id.settings -> {
+                    //binding.cvMiniPlayer.visibility = View.GONE
+                    replaceFragment(SettingsFragment())
+                }
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
